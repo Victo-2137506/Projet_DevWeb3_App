944e83515b4b52e067b62fbcb507388ee951e646
Victo-2137506 <2137506@carrefour.cegepvicto.ca>
2025-12-15 14:00:59 -0500

    test

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t01kubv3lko"
+    "revision": "0.qd7mrepghqg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
